2a26ec2a3b0b2a4fea044f0e2f5092c4dc74ae68
Benny Johnson <BtpPrograms@gmail.com>
2016-01-23 21:13:19 -0500

    Added gitignore and wrote basic pose data reader

---
+++ myo_output_test.py
@@ -0,0 +1,14 @@
+import myo
+import sys
+
+m = myo.Myo(myo.NNClassifier(), sys.argv[1] if len(sys.argv) >= 2 else None)
+
+def data_print(data):
+        print (data)
+
+m.add_raw_pose_handler(data_print)
+
+m.connect()
+
+while True:
+    m.run()
